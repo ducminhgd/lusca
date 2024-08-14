7d065a39da6e15b9b3b6249fb73941bc38273f5c
Giã Dương Đức Minh <giaduongducminh@gmail.com>
2024-08-14 13:50:04 +0700

    feat(models): before create for UUID

--- cmd/demodb/main.go
+++ cmd/demodb/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/ducminhgd/lusca/config"
 	"github.com/ducminhgd/lusca/internal/models"
 	"github.com/ducminhgd/lusca/internal/require"
-	"github.com/google/uuid"
 	"gorm.io/plugin/dbresolver"
 )
 
@@ -19,11 +16,7 @@ func main() {
 		panic(err)
 	}
 	db := dbManager.DB()
-
-	uuidv7, _ := uuid.NewV7()
-	fmt.Println(uuidv7)
 	f := models.Feature{
-		ID:          uuidv7,
 		Name:        "feature-2",
 		Description: "feature-2 description",
 		Status:      1,
--- internal/models/feature.go
+++ internal/models/feature.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Feature struct {
@@ -12,9 +13,19 @@ type Feature struct {
 	Description string    `json:"description" gorm:"column:description"`
 	Status      int       `json:"status" gorm:"column:status"`
 	CreatedAt   time.Time `json:"createdAt" gorm:"column:created_at"`
-	UpdatedAt   time.Time `json:"updatedAt" gorm:"column:updated_at"`
+	UpdatedAt   time.Time `json:"updatedAt" gorm:"column:updated_at;autoUpdateTime"`
 }
 
 func (Feature) TableName() string {
 	return "feature"
+}
+
+func (m *Feature) BeforeCreate(tx *gorm.DB) error {
+	id, err := uuid.NewV7()
+	if err != nil {
+		return err
+	}
+
+	m.ID = id
+	return nil
 }
